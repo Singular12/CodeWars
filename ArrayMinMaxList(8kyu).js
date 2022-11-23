863d49557d24ff4074a72517fa0996e140193f05
Andrew Padgett <101520702+Singular12@users.noreply.github.com>
2022-11-23 10:00:17 -0800

    Create ArrayMinMaxList(8kyu).js

---
+++ ArrayMinMaxList(8kyu).js
@@ -0,0 +1,9 @@
+let min = function(list){
+    return Math.min(...list);
+}
+
+let max = function(list){
+    return Math.max(...list);
+}
+
+// Simple method for returning min and max of values passed in function.
