ce4ccc287cb6fed986ee1eb9b6107930f3695dfe
Andrew Padgett <101520702+Singular12@users.noreply.github.com>
2022-11-24 13:06:59 -0800

    Create Cuboid(8kyu).js

---
+++ Cuboid(8kyu).js
@@ -0,0 +1,13 @@
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    return length * width * height;
+  }
+}
+
+// Cuboid formula solution in CodeWars.
+
+cuboidFunction = (length, width, height) => {return length * width * height}
+cuboidFunction(2, 2, 2)
+
+
+// Arrow function cuboid formula.  Probably better.
