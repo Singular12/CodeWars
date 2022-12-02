634426a629b8d02b2a8d06f1b9ad841253bc6e67
Andrew Padgett <101520702+Singular12@users.noreply.github.com>
2022-12-01 16:51:35 -0800

    Create OppositeNumbers(8kyu).js

---
+++ OppositeNumbers(8kyu).js
@@ -0,0 +1,8 @@
+// To draw an opposite number.  Arrow function format.
+const opposite = number => -number;
+
+// Or with regular function format.
+
+function opposite(number) {
+  return -number;
+}
