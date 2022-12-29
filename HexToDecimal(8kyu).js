c9d8fee1db7c8beb98914319eb59918e90f7529a
Andrew Padgett <101520702+Singular12@users.noreply.github.com>
2022-12-29 11:05:27 -0800

    Create HexToDecimal(8kyu).js

---
+++ HexToDecimal(8kyu).js
@@ -0,0 +1,5 @@
+// Complete the function which converts hex number (given as a string) to a decimal number.
+
+const hexToDec = hex => parseInt(hex, 16);
+
+// parseInt method takes a string and converts to an integer
