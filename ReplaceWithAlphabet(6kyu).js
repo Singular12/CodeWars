f31e2692269e9d7608690cf15c23433959ab340c
Andrew Padgett <101520702+Singular12@users.noreply.github.com>
2023-01-05 08:45:03 -0800

    Create ReplaceWithAlphabet(6kyu).js

---
+++ ReplaceWithAlphabet(6kyu).js
@@ -0,0 +1,11 @@
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+function alphabetPosition(text) {
+  var result = "";
+  for (var i = 0; i < text.length; i++) {
+    var code = text.toUpperCase().charCodeAt(i)
+    if (code > 64 && code < 91) result += (code - 64) + " ";
+  }
+
+  return result.slice(0, result.length - 1);
+}
